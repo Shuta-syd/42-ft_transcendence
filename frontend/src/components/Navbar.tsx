715270cbaf246ecebb1d36ac350b9d2de0b31048
Shuta-syd <shuta.ogura0921@gmail.com>
2023-02-07 19:25:44 +0900

    #7 [add] ant design setting fix & Chat.tsx add

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <h2>
+        This is Navbar
+      </h2>
+    </div>
+  );
+}
+
+export default Navbar;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Input } from 'antd';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
-    <Input/>
+    <Navbar />
   </React.StrictMode>
 );
