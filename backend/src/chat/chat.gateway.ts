fb000b620c506d45432dcd909198a4fd02b774a5
Shuta-syd <shuta.ogura0921@gmail.com>
2023-02-09 19:16:49 +0900

    #7 [add] ChatGateway class add, WebsokcetContext.tsx add

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,9 +1,23 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @WebSocketGateway()
 export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('send_message')
+  listenForMessages(@MessageBody() data: string) {
+    this.server.sockets.emit('receive_message', data);
+  }
+
+  @SubscribeMessage('events')
+  handleEvent(@MessageBody() data: string): string {
+    return data;
   }
 }
