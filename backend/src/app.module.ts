1f21a5ff53a956cfb2e890eb3671204b2c127408
Shuta-syd <shuta.ogura0921@gmail.com>
2023-02-09 16:32:15 +0900

    #7 [fix] @nestjs/websokcets, socket.io version changed

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatGateway } from './chat/chat.gateway';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [ChatModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ChatGateway],
 })
 export class AppModule {}
