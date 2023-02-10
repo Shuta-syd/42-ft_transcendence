143774f291204a1445f36b465d026af251edb2db
Shuta-syd <shuta.ogura0921@gmail.com>
2023-02-10 18:45:06 +0900

    #7 [done] basic chat system done

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,31 +1,57 @@
+import { Logger } from '@nestjs/common';
 import {
+  ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+
+type ChatPayload = {
+  uname: string;
+  time: string;
+  text: string;
+};
 
 @WebSocketGateway({
   cors: {
     origin: ['http://localhost:3000'],
   },
 })
-export class ChatGateway {
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer()
   server: Server;
+  private logger: Logger = new Logger('ChatGateway');
 
-  onModuleInit() {
-    this.server.on('connection', (socket) => {
-      console.log('Connected', socket.id);
+  @SubscribeMessage('chatToServer') // to subscribeEvent
+  //@MessageBody clientから送られてくるbody内容
+  handleMessage(
+    @MessageBody() payload: ChatPayload,
+    @ConnectedSocket() client: Socket,
+  ) {
+    this.logger.log('Chat Received');
+    this.logger.log(payload);
+    this.server.emit('chatToClient', {
+      ...payload,
+      socketId: client.id,
     });
   }
 
-  @SubscribeMessage('send_message') // to subscribeEvent
-  //@MessageBody clientから送られてくるbody内容
-  listenForMessages(@MessageBody() data: string) {
-    this.server.emit('onMessage', {
-      content: data,
-    });
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected ${client.id}`);
   }
 }
--- frontend/src/components/Websocket.tsx
+++ frontend/src/components/Websocket.tsx
@@ -1,53 +1,78 @@
 import { Button, TextField } from "@mui/material";
-import React, { useContext, useEffect, useState } from "react"
+import { Stack } from "@mui/system";
+import React, { useCallback, useContext, useEffect, useState } from "react"
 import { WebsocketContext } from "../contexts/WebsocketContext"
 
 type MessagePayload = {
   socketId: string;
-  content: string;
+  uname: string;
+  time: string;
+  text: string;
 };
 
+type ChatLog = MessagePayload[];
+
 export default function Websocket() {
-  const [value, setValue] = useState('');
-  const [messages, setMessage] = useState<MessagePayload[]>([]);
+  const [text, setText] = useState('');
+  const [uname, setUname] = useState('');
+  const [chatLog, setChatLog] = useState<ChatLog>([]);
   const socket = useContext(WebsocketContext); // type is Socket
 
   useEffect(() => {
     socket.on('connect', () => {
-      socket.on('onMessage', (newMessage : MessagePayload) => {
-        setMessage((prev) => [...prev, newMessage]);
-      });
+      console.log(`接続ID: ${socket.id}`);
     });
 
     return () => {
-      socket.off('connect');
-      socket.off('onMessage');
+      console.log(`切断: ${socket.id}`);
+      socket.disconnect();
     }
   }, [])
 
-  const onSubmit = () => {
-    socket.emit('send_message', value);
-    setValue('');
-  }
+  useEffect(() => {
+    socket.on('chatToClient', (chat: MessagePayload) => {
+      console.log(`チャット受信:`, chat);
+      const newChatLog = [...chatLog, chat];
+      setChatLog(newChatLog);
+    });
+  }, [chatLog])
+
+  const getNow = useCallback((): string => {
+    const date = new Date();
+    return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}
+     ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}
+    `
+  }, []);
+
+  const sendChat = useCallback(() => {
+    if (!uname) {
+      alert('ユーザ名を入力してください');
+      return;
+    }
+    console.log('送信');
+    socket.emit('chatToServer', { uname, text, time: getNow() })
+    setText('');
+  }, [uname, text]);
 
   return (
     <div>
       <h1>Websocket Component</h1>
-      <div>
-        {messages.length === 0 ?
-          <div>No Message</div> :
-          <div>
-            {messages.map((msg, key) => <div key={key}>{msg.content}</div>)}
-          </div>
-        }
-      </div>
-      <TextField fullWidth label='...' variant="outlined"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <Button variant="contained"
-        onClick={onSubmit}
-      >Emit</Button>
+      <h4>ユーザ名</h4>
+      <TextField value={uname} onChange={(e) => { setUname(e.target.value) }} />
+
+      <h3>トーク内容</h3>
+      <Stack spacing={1}>
+        {chatLog.map((chat, idx) => (
+            <div key={idx}>
+              <div>{chat.time}[{chat.socketId}]</div>
+              <div>[{chat.uname}]: {chat.text}</div>
+            </div>
+          ))}
+      </Stack>
+
+      <h3>送信内容</h3>
+      <TextField fullWidth value={text} onChange={(e) => { setText(e.target.value) }} />
+      <Button variant="contained" onClick={sendChat}>Emit</Button>
     </div>
   )
 }
