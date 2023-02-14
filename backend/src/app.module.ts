bdf723109ee02efa6f20420ea5ac48e3aba6c987
public-jun <generalpublicjunjun@gmail.com>
2023-02-14 09:08:43 +0900

    #16 [add] micropost app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
+import { PrismaService } from './prisma/prisma.service';
+import { UserService } from './user/user.service';
+import { PostService } from './post/post.service';
 
 @Module({
   imports: [ChatModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserService, PostService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8080);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 }
 bootstrap();
