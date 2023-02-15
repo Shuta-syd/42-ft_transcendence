6f3d348f7f0583a50e93f11eb3b75b47b029b9b7
Shuta-syd <shuta.ogura0921@gmail.com>
2023-02-15 19:48:23 +0900

    #14 [add] prisma service, module, controller add

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaController } from './prisma/prisma.controller';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ChatModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ChatModule, PrismaModule],
+  controllers: [AppController, PrismaController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //app.userGlobalPipes(new ValidationPipe({ whitelist: true }} )) //dto, validateで使用
+  app.enableCors({
+    origin: ['http://localhost:3000'], //許可したいfrontsideのURL
+  });
   await app.listen(8080);
 }
 bootstrap();
