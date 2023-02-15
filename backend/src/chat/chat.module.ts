5cef5811a4f7166563bec80b43f194e3baee67c6
Shuta-syd <shuta.ogura0921@gmail.com>
2023-02-15 19:28:54 +0900

    #14 [add] chat service, controller add

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
+import { ChatService } from './chat.service';
+import { ChatController } from './chat.controller';
 
 @Module({
-  providers: [ChatGateway],
+  providers: [ChatGateway, ChatService],
+  controllers: [ChatController],
 })
 export class ChatModule {}
