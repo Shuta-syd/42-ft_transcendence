b97abed796781df7d42dff8eb897f4421482ddf5
Shuta-syd <shuta.ogura0921@gmail.com>
2023-02-16 15:54:32 +0900

    #14 [add] signUp API add using prisma client

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ChatModule, PrismaModule, UserModule],
+  imports: [ChatModule, PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,16 @@
+import { Body, Get, Param, Post } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Prisma, User } from '@prisma/client';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async signupUser(
+    @Body() userData: { name: string; email: string; password: string },
+  ): Promise<User> {
+    return this.userService.createUser(userData);
+  }
+}
