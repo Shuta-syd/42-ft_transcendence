5bc44c93e33030db35853ebbe77d954fd11ebff9
Shuta-syd <shuta.ogura0921@gmail.com>
2023-02-17 16:34:37 +0900

    #14 [update] Successfully displayed database using useQuery

--- frontend/src/components/sample/ReactQuery.tsx
+++ frontend/src/components/sample/ReactQuery.tsx
@@ -1,25 +1,16 @@
 import React from "react";
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { useQuery } from "react-query";
+import useQueryChat from "../../hooks/chat/useQueryChat";
 
-const fetchUsers = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  return res.json();
-}
 
 function ReactQueryComponent() {
-  const ret = useQuery('users', fetchUsers);
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { data } = ret;
+  const { data } = useQueryChat(1);
+  console.log(data);
   return (
     <>
       <h2>ユーザ一覧</h2>
       <div>
-        {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-          data.map((user: any) => (
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
-          <div key={user.id}>{user.name}</div>
+        {data?.map((obj, idx) => (
+          <div key={obj.id}>{obj.message}</div>
         ))}
       </div>
     </>
--- frontend/src/features/game/Game.tsx
+++ frontend/src/features/game/Game.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { QueryClient, QueryClientProvider } from "react-query";
-import FetchComponent from "../../components/sample/fetch";
 import ReactQueryComponent from "../../components/sample/ReactQuery";
 
 const queryClient = new QueryClient();
@@ -9,8 +7,7 @@ const queryClient = new QueryClient();
 function Game() {
   return (
     <QueryClientProvider client={queryClient}>
-      <FetchComponent />
-      {/* <ReactQueryComponent /> */}
+      <ReactQueryComponent />
     </QueryClientProvider>
   );
 }
