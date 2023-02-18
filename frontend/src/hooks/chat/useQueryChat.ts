b31b2a6ca5651e3a25e13b41bc3613241c557cfc
Shuta-syd <shuta.ogura0921@gmail.com>
2023-02-18 19:26:09 +0900

    #30 [update] prisma User model update, add Friend

--- frontend/src/hooks/chat/useQueryChat.ts
+++ frontend/src/hooks/chat/useQueryChat.ts
@@ -6,14 +6,14 @@ import { Message } from "../../types/PrismaType";
 function useQueryChat(roomId: string) {
   const router = useNavigate();
 
-  const getChatlog = async () => {
+  const getChatLog = async () => {
     const { data } = await axios.get<Message[]>(`http://localhost:8080/chat/room/${roomId}`);
     return data;
   }
 
   return useQuery<Message[], Error>({
     queryKey: `chatRoom${String(roomId)}`,
-    queryFn: getChatlog,
+    queryFn: getChatLog,
     onError: (err: any) => {
       if (err.response.status === 401 || err.response.status === 403)
         router('/chat');
