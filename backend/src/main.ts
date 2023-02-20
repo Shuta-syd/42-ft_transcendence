4ccb70dec285f66158b3d3097bc6fc6678172735
Shuta-syd <shuta.ogura0921@gmail.com>
2023-02-20 18:00:48 +0900

    #29 [add] nestjs swagger setup add

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('ft_transcendence')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   //app.userGlobalPipes(new ValidationPipe({ whitelist: true }} )) //dto, validateで使用
   app.enableCors({
     origin: ['http://localhost:3000'], //許可したいfrontsideのURL
