ba696a5a4cae1ac792091d432ba1efe50a67e453
Shuta-syd <shuta.ogura0921@gmail.com>
2023-02-20 17:04:42 +0900

    #30 [update] Update to display the user's friends in a list

--- frontend/src/components/chat/ChatFriendsComponent.tsx
+++ frontend/src/components/chat/ChatFriendsComponent.tsx
@@ -1,28 +1,45 @@
 import { Avatar, Grid, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 import PersonIcon from '@mui/icons-material/Person';
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useQueryFriend from "../../hooks/user/useQueryFriend";
+
+type FriendPayload = {
+  id: number;
+  name: string;
+}
+
 
 /**
  * @returns DirectMessage送信可能なフレンド一覧を表示するコンポーネント
  */
 export default function ChatFriendsComponent() {
-  const friends = SampleFriend;
+  const UserID = '1'; // tmp
+  const { data } = useQueryFriend(UserID);
+  const [friends, setFriends] = useState<FriendPayload[]>([]);
+
+  useEffect(() => {
+    setFriends([]);
+    if (data) {
+      data?.map((obj) => {
+        const friend: FriendPayload = { id: obj.id, name: obj.name };
+        setFriends(prevFriends => [...prevFriends, friend]);
+      })
+    }
+  }, [data]);
+
   return (
     <Stack spacing={2} sx={{ backgroundColor: '#d1c4e9' }} height={'91vh'}>
-      {friends.map((friend, idx) => (
+      {friends?.map((friend, idx) => (
         <Grid container key={idx}>
           <Grid item mr={2}>
             <Avatar ><PersonIcon /></Avatar>
           </Grid>
           <Grid item>
-            <Typography variant="subtitle1">{friend}</Typography>
+            <Typography variant="subtitle1">{friend.name}</Typography>
           </Grid>
         </Grid>
       ))}
     </Stack>
   )
 }
-
-
-const SampleFriend: string[] = ['user1', 'user2', 'user3'];
