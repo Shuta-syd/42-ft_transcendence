2b4b9e623d8fe8e43333ce16a1b7f6f3569f9d93
Shuta-syd <shuta.ogura0921@gmail.com>
2023-02-20 20:13:27 +0900

    #30 [fix] App.tsx fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import {Route, Routes } from 'react-router-dom';
 import Home from "./features/home/Home";
 import Chat from "./features/chat/Chat";
-import Game from "./features/game/Game";
 import ChatComponent from "./components/chat/ChatComponent";
 import ChatWindowComponent from "./components/chat/ChatWindowComponent";
+import Canvas from "./features/game/Game";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
               <Route path=":roomId" element={<ChatWindowComponent />} />
             </Route>
           </Route>
-          <Route path="/game" element={<Game/>} />
+          <Route path="/game" element={<Canvas/>} />
         </Routes>
     </>
   )
