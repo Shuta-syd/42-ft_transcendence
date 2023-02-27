424d48179a73843480a442689b25ac73b08aa09d
kny-i <kny_i@keio.jp>
2023-02-28 07:32:38 +0900

    #49[add] server side basic gateway response

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -1,9 +1,46 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  MessageBody,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
 export class GameGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  @WebSocketServer()
+  server: Server;
+  wsClients = [];
+
+  handleConnection(client: any) {
+    this.wsClients.push(client);
   }
+
+  @SubscribeMessage('game')
+  chat(@MessageBody() data: any) {
+    console.log(data);
+    this.broadcast('chat', data.message);
+  }
+
+  @SubscribeMessage('game_test')
+  emitLoginMessage(@MessageBody() data: any) {
+    console.log(data);
+    this.broadcast('login', data + 'さんがログインしました。');
+  }
+
+  private broadcast(event, message: string) {
+    const broadCastMessage = message;
+    for (let c of this.wsClients) {
+      c.emit(event, broadCastMessage);
+    }
+  }
+
+  // @SubscribeMessage('message')
+  // handleMessage(client: any, payload: any): string {
+  //   return 'Hello world!';
+  // }
 }
