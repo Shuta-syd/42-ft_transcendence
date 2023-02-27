7fb4ef0634e122985e32be432ad6740eb7fe068d
Shuta-syd <shuta.ogura0921@gmail.com>
2023-02-27 19:50:39 +0900

    #55 [add] backend jwt passport package add

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [PrismaModule, ConfigModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
