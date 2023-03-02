bf815cd017921feeb2062944ccdd28a636c0b788
vim <cant_reply@mail.com>
2023-03-03 06:15:01 +0900

    #58[add] GamePlayer1で1の動作を実現

--- frontend/src/features/game/Game.tsx
+++ frontend/src/features/game/Game.tsx
@@ -3,223 +3,144 @@ import useQueryUserGame from "../../hooks/game/useQueryGame";
 import { User } from "../../types/PrismaType";
 import { GameSocket } from "../../contexts/WebsocketContext";
 
-// global variables
-let context: CanvasRenderingContext2D | null;
-let canvas:  HTMLCanvasElement | null;
-let keycode = '';
-let leftScore = 0;
-let rightScore = 0;
+const Game = () => {
+    // global variables
+    let context: CanvasRenderingContext2D | null;
+    let canvas:  HTMLCanvasElement | null;
+    let keycode = '';
+    let leftScore = 0;
+    let rightScore = 0;
 
-/* Ball macro */
-const BALLX = 455;
-const BALLY = 450;
-const RADIUS = 25;
+    /* Ball macro */
+    const BALLX = 455;
+    const BALLY = 450;
+    const RADIUS = 25;
 
-/* Paddle macro */
+    /* Paddle macro */
 
-const PADDLEWIDTH = 20;
-const PADDLEWHEIGHT = 200;
+    const PADDLEWIDTH = 20;
+    const PADDLEWHEIGHT = 200;
 
-/* Field macro */
-const FIELDX = 5;
-const FIELDY = 100;
-const FIELDWIDTH = 900;
-const FIELDHEIGHT = 700;
-const MIDDLEX = 450;
+    /* Field macro */
+    const FIELDX = 5;
+    const FIELDY = 100;
+    const FIELDWIDTH = 900;
+    const FIELDHEIGHT = 700;
+    const MIDDLEX = 450;
 // const MIDDLEY = 450;
 
-/* Left Paddle macro */
-const LPADDLEX = 5;
-const LPADDLEY = 100;
+    /* Left Paddle macro */
+    const LPADDLEX = 5;
+    const LPADDLEY = 100;
 
-/* Right Paddle macro */
-const RPADDLEX = FIELDX + FIELDWIDTH - PADDLEWIDTH;
-const RPADDLEY = 100;
+    /* Right Paddle macro */
+    const RPADDLEX = FIELDX + FIELDWIDTH - PADDLEWIDTH;
+    const RPADDLEY = 100;
 
-/* Display macro */
+    /* Display macro */
 
-const WIDTH = 1000;
-const HEIGHT = 900;
+    const WIDTH = 1000;
+    const HEIGHT = 900;
 
-/*
-ballの情報をオブジェクト化して、drawで描けるようになってる
--> ballのx, yを更新できるようにしていく
- */
+    /*
+    ballの情報をオブジェクト化して、drawで描けるようになってる
+    -> ballのx, yを更新できるようにしていく
+     */
 
 // const ANONYMOUS = 0;
-const PLAYER1 = 1;
-const PLAYER2 = 2;
-const OBSERVER = 3;
+    const PLAYER1 = 1;
+    const PLAYER2 = 2;
+    const OBSERVER = 3;
 
 // let PlayerType = ANONYMOUS;
 
 
-const ball = {
-    x: BALLX,
-    y: BALLY,
-    /* vx/vyはあくまで最初の段階での動きをrandomにしているだけ */
-    vx: Math.cos(randomInt(0, 30) * (Math.PI / 180)) * 8,
-    vy: Math.sin(randomInt(0, 30) * (Math.PI / 180)) * 8,
-    radius: RADIUS,
-    color: "black",
-    draw() {
-        context?.beginPath();// 自身を書く関数をpropertyのなかに格納
-        context?.arc(this.x, this.y, this.radius, 0, Math.PI * 2 );
-        context?.closePath();
-        context?.fillStyle && (context.fillStyle = this.color);
-        context?.fill();
-    },
-    init(){
-        this.x = BALLX;
-        this.y = BALLY;
-        this.vx = Math.cos(randomInt(0, 360) * (Math.PI / 180)) * 8;
-        this.vy = Math.sin(randomInt(0, 360) * (Math.PI / 180)) * 8;
+    const ball = {
+        x: BALLX,
+        y: BALLY,
+        /* vx/vyはあくまで最初の段階での動きをrandomにしているだけ */
+        vx: Math.cos(randomInt(0, 30) * (Math.PI / 180)) * 8,
+        vy: Math.sin(randomInt(0, 30) * (Math.PI / 180)) * 8,
+        radius: RADIUS,
+        color: "black",
+        draw() {
+            context?.beginPath();// 自身を書く関数をpropertyのなかに格納
+            context?.arc(this.x, this.y, this.radius, 0, Math.PI * 2 );
+            context?.closePath();
+            context?.fillStyle && (context.fillStyle = this.color);
+            context?.fill();
+        },
+        init(){
+            this.x = BALLX;
+            this.y = BALLY;
+            this.vx = Math.cos(randomInt(0, 360) * (Math.PI / 180)) * 8;
+            this.vy = Math.sin(randomInt(0, 360) * (Math.PI / 180)) * 8;
+        }
+    };
+
+
+    const leftPaddle = {
+        x: LPADDLEX,
+        y: LPADDLEY,
+        color: "black",
+        draw() {
+            context?.beginPath();
+            context?.rect(this.x, this.y, PADDLEWIDTH, PADDLEWHEIGHT);
+            context?.closePath();
+            context?.fillStyle && (context.fillStyle = this.color);
+            context?.fill();
+        }
     }
-};
 
 
-const leftPaddle = {
-    x: LPADDLEX,
-    // get y() {
-    //     return LeftPaddlePos;
-    // },
-    // set y(value) {
-    //     setLeftPaddlePos(value);
-    // },
-    y: LPADDLEY,
-    color: "black",
-    draw() {
-        context?.beginPath();
-        context?.rect(this.x, this.y, PADDLEWIDTH, PADDLEWHEIGHT);
-        context?.closePath();
-        context?.fillStyle && (context.fillStyle = this.color);
-        context?.fill();
+    const rightPaddle = {
+        x: RPADDLEX,
+        y: RPADDLEY,
+        color: "black",
+        draw() {
+            context?.beginPath();
+            context?.rect(this.x, this.y, PADDLEWIDTH, PADDLEWHEIGHT);
+            context?.closePath();
+            context?.fillStyle && (context.fillStyle = this.color);
+            context?.fill();
+        }
     }
-}
 
-
-const rightPaddle = {
-    x: RPADDLEX,
-    y: RPADDLEY,
-    color: "black",
-    draw() {
-        context?.beginPath();
-        context?.rect(this.x, this.y, PADDLEWIDTH, PADDLEWHEIGHT);
-        context?.closePath();
-        context?.fillStyle && (context.fillStyle = this.color);
-        context?.fill();
+    /* Helper */
+    function randomInt(min: number, max: number): number {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-}
-
-/* Helper */
-function randomInt(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 
-/*
-後に変更のないobject
-(ex) line
- */
-function drawStaticObject() {
-    // create a field of game
-    /* lineを出す */
-    context?.beginPath();
     /*
-    rectangleの外枠だけを出力する関数
-    x、 y(始点)、幅、高さ
+    後に変更のないobject
+    (ex) line
      */
-    context?.strokeRect(FIELDX, FIELDY, FIELDWIDTH, FIELDHEIGHT);
+    function drawStaticObject() {
+        // create a field of game
+        /* lineを出す */
+        context?.beginPath();
+        /*
+        rectangleの外枠だけを出力する関数
+        x、 y(始点)、幅、高さ
+         */
+        context?.strokeRect(FIELDX, FIELDY, FIELDWIDTH, FIELDHEIGHT);
+        /*
+        strokeを用いて、設定情報からlineをひく
+         */
+        context?.beginPath();
+        context?.moveTo(MIDDLEX, FIELDY);
+        context?.lineTo(MIDDLEX, FIELDWIDTH - 100);
+        context?.stroke();
+    }
+
     /*
-    strokeを用いて、設定情報からlineをひく
+    後があるobject
      */
-    context?.beginPath();
-    context?.moveTo(MIDDLEX, FIELDY);
-    context?.lineTo(MIDDLEX, FIELDWIDTH - 100);
-    context?.stroke();
-}
-
-/*
-後があるobject
- */
-type BallPos = {
-    x: number;
-    y: number;
-};
-
-/*
-function draw(PlayerType: number) {
-    context?.clearRect(0, 0, canvas?.width || 0, canvas?.height || 0);
-    drawStaticObject();
-
-    if (PlayerType !== PLAYER1) {
-        /!* check collision *!/
-        if (ball.x - ball.radius <= leftPaddle.x + PADDLEWIDTH
-            && (ball.y <= leftPaddle.y + PADDLEWHEIGHT
-                && ball.y >= leftPaddle.y)){
-            ball.vx = -ball.vx;
-        }else if (ball.x + ball.radius >= rightPaddle.x
-            && (ball.y <= rightPaddle.y + PADDLEWHEIGHT
-                && ball.y >= rightPaddle.y)) {
-            ball.vx = -ball.vx;
-        } else if (FIELDHEIGHT + FIELDY < ball.y || ball.y < FIELDY) {
-            ball.vy = -ball.vy;
-        } else if (ball.x < FIELDX) {
-            rightScore += 1;
-            ball.init();
-        } else if (FIELDX + FIELDWIDTH < ball.x) {
-            leftScore += 1;
-            ball.init();
-        }
-
-    }
-
-    /!* check keycode *!/
-    if (keycode === 'KeyW') {
-        if (rightPaddle.y  > FIELDY) {
-            rightPaddle.y -= 50;
-            GameSocket.emit('GameToServer', rightPaddle.y);
-        }
-    }
-    if (keycode === 'KeyS') {
-        if (rightPaddle.y + PADDLEWHEIGHT < FIELDHEIGHT + FIELDY) {
-            rightPaddle.y += 50;
-            GameSocket.emit('GameToServer', rightPaddle.y);
-        }
-    }
-    keycode = '';
-
-
-    /!* send ball pos to server *!/
-    if (PlayerType === PLAYER1) {
-        ball.x += ball.vx;
-        ball.y += ball.vy;
-        const BallPos:BallPos = {
-            x: ball.x,
-            y:ball.y,
-        }
-        GameSocket.emit('BallPosToServer', BallPos);
-    }
-
-    /!* draw part *!/
-    leftPaddle.draw();
-    rightPaddle.draw();
-    ball.draw();
-    if (canvas == null || context == null) {
-        return ;
-    }
-
-    context.fillStyle = 'black';
-    context.font = "bold 50px 'ＭＳ 明朝'";
-    context.fillText(leftScore.toString() , 360, 50);
-    context.fillText( '-', 440, 50);
-    context.fillText( rightScore.toString(), 500, 50);
-    window.requestAnimationFrame(draw);
-} */
-
-
-
-const Game = () => {
+    type BallPos = {
+        x: number;
+        y: number;
+    };
 
 
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -340,7 +261,6 @@ const Game = () => {
         time: string
         text: string
     }
-
 
     type ChatLog = Array<Chat>
 
