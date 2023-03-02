cdc6d95cebbb4281b3d0921b215ccb9b5320c407
kny-i <kny_i@keio.jp>
2023-03-03 04:04:51 +0900

    "#58[add] add Routes of  Player1 Player2 Observer"

---
+++ frontend/src/features/game/GamePlayer1.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GamePlayer1 = () => {
+    console.log("hoge");
+    return (
+        <h1>Player1</h1>
+    );
+}
+
+
+export default GamePlayer1;
---
+++ frontend/src/features/game/GamePlayer2.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const GamePlayer2 = () => (
+    <h1>Player2</h1>
+)
+
+
+export default GamePlayer2;
