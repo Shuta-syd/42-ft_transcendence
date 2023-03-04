8ef4f362b7941dd9c858742e21aa3c9f2f9ebec5
Shuta-syd <shuta.ogura0921@gmail.com>
2023-03-04 17:00:05 +0900

    #13 [fix] WebsocketContext unused var deleted

--- frontend/src/contexts/WebsocketContext.tsx
+++ frontend/src/contexts/WebsocketContext.tsx
@@ -1,6 +1,3 @@
+/* eslint-disable no-unused-vars */
 import { createContext } from "react";
 import { io, Socket } from "socket.io-client";
-
-export const socket = io('http://localhost:8080/chat')
-export const WebsocketContext = createContext<Socket>(socket);
-export const WebsocketProvider = WebsocketContext.Provider;
