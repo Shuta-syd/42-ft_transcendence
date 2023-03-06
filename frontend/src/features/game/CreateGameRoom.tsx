84cc07c3e5daa6da35ea8fe81c1ee60c73a248f1
kny-i <kny_i@keio.jp>
2023-03-07 03:16:01 +0900

    #58[add] add tsx implement still wip next step is creating interface of going to game part with data of room id and am i Player1 or not

--- frontend/src/features/game/CreateGameRoom.tsx
+++ frontend/src/features/game/CreateGameRoom.tsx
@@ -1,6 +1,12 @@
 import React, {useEffect, useState, useRef} from "react";
+import {Link} from "react-router-dom";
 import {Game, User} from "../../types/PrismaType";
 import useGameUser, { GameRoomReq } from '../../hooks/game/useGameuser';
+
+
+// let PlayerType: number;
+// let roomId: number;
+
 
 const CreateGameRoom = () => {
 
@@ -20,17 +26,43 @@ const CreateGameRoom = () => {
     useEffect(() => {
         gamePromisesRef.current?.then((Gamedto: Game) => {
             setGame(Gamedto);
+            // if (Gamedto.player2.length === 0)
+            //     PlayerType = 1;
+            // else
+            //     PlayerType = 2;
+            // roomId = Gamedto.id;
         });
     }, [user]);
 
+
+  /*  function Ready() {
+        if (PlayerType === 1)
+            return(
+                <div>
+                    <Link to={"/game/player1"}>Player1</Link>
+                </div>
+            );
+        if (PlayerType === 2)
+            return(
+                <div>
+                    <Link to={"/game/player2"}>Player2</Link>
+                </div>
+            );
+    } */
     return (
         <div>
             <h1>[Create Game]</h1>
             <h2>You are {user?.name}!!!</h2>
             <h2>You are in {game?.id}!!!</h2>
-
+            <h2>Player1 is  {game?.player1.valueOf()}!!!</h2>
+            <h2>Player2 is  {game?.player2.valueOf()}!!!</h2>
             <h2>Wating for someone </h2>
-            <p></p>
+            <div>
+                <Link to={"/game/player1"}>Player1</Link>
+            </div>
+            <div>
+                <Link to={"/game/player2"}>Player2</Link>
+            </div>
         </div>
     )
 }
