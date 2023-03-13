87b1ee110ab5c587b0ff10f49af56d7719e9bcc4
Shuta-syd <shuta.ogura0921@gmail.com>
2023-03-13 22:37:38 +0900

    #61 [update] NewNavbar.tsx update

--- frontend/src/components/utils/NewNavbar.tsx
+++ frontend/src/components/utils/NewNavbar.tsx
@@ -1,48 +1,95 @@
 /* eslint-disable no-unused-vars */
 import { Avatar, Box, IconButton, Stack } from "@mui/material";
 import Looks4Icon from '@mui/icons-material/Looks4';
-import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
-import ForumIcon from '@mui/icons-material/Forum';
+import VideogameAssetOutlinedIcon from '@mui/icons-material/VideogameAssetOutlined';
+import ForumOutlinedIcon from '@mui/icons-material/ForumOutlined';
+import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
 import PersonIcon from '@mui/icons-material/Person';
-import ChatIcon from '@mui/icons-material/Chat';
 import React from "react";
+import '../../styles/Navbar.css'
+import { Link } from "react-router-dom";
 
 export default function NewNavBar() {
   return (
     <>
       <Box
-        width={'5vw'} height={'100vh'}
+        width={'6rem'} height={'100vh'}
         sx={{
           backgroundColor: '#ffffff',
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
+          justifyContent: 'space-between'
         }}
         borderRight={1}
       >
-        <Avatar>
-          <Looks4Icon fontSize="large"/>
-        </Avatar >
         <Box
+          marginTop={'3vh'}
+        >
+          <Link to={'/'}>
+            <IconButton>
+              <Looks4Icon
+                fontSize="large"
+                sx={{color: '#B2B9C5'}}
+                />
+            </IconButton>
+          </Link>
+        </Box>
+        <Stack
+          spacing={7}
           sx={{
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
           }}
         >
-          <IconButton>
-            <VideogameAssetIcon fontSize="large" />
-          </IconButton>
-          <IconButton>
-            <ChatIcon fontSize="large"/>
-          </IconButton>
-          <IconButton>
-            <ForumIcon fontSize="large" />
-          </IconButton>
+          <Box
+            className={'NavbarActive'}
+            borderRadius={4}
+          >
+            <Link to={'/game'}>
+              <IconButton>
+                <VideogameAssetOutlinedIcon
+                  fontSize="large"
+                  sx={{ color: '#1f9f88' }}
+                  className={'NavbarButton'}
+                  />
+              </IconButton>
+            </Link>
+          </Box>
+          <Box>
+            <Link to={'/chat/room'}>
+              <IconButton>
+                <ChatOutlinedIcon
+                  fontSize="large"
+                  sx={{color: '#B2B9C5'}}
+                  />
+              </IconButton>
+            </Link>
+          </Box>
+          <Box>
+            <Link to={'/channel/room'}>
+              <IconButton>
+                <ForumOutlinedIcon
+                  fontSize="large"
+                  sx={{color: '#B2B9C5'}}
+                  />
+              </IconButton>
+            </Link>
+          </Box>
+        </Stack>
+        <Box
+          height={'5vh'}
+          marginBottom={'3vh'}
+        >
+          <Link to={'/user'}>
+            <IconButton>
+                <PersonIcon
+                  sx={{color: '#B2B9C5'}}
+                  />
+            </IconButton>
+          </Link>
         </Box>
-        <Avatar>
-          <PersonIcon/>
-        </Avatar>
       </Box>
     </>
   )
