27524ab17093fd8c3e78ec6dad596f38112f1ab0
kny-i <kny_i@keio.jp>
2023-03-20 06:02:38 +0900

    #121[add] add get match from profile

--- frontend/src/features/profile/Profile.tsx
+++ frontend/src/features/profile/Profile.tsx
@@ -5,9 +5,10 @@ import {deepPurple} from "@mui/material/colors";
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-import { User } from "../../types/PrismaType";
+import {Match, User} from "../../types/PrismaType";
 import { useProfileUser } from "../../hooks/profile/useProfileUser";
 import { sendFriendRequest } from "../../hooks/profile/sendFriendRequests";
+import useQueryMatches from "../../hooks/match/useWueryMatch";
 
 const Profile = () => {
     const [user, setUser] = useState<User>();
@@ -28,6 +29,79 @@ const Profile = () => {
         sendFriendRequest(user?.id, inputId);
         console.log('inputid => ', inputId);
         console.log('  my id => ', user?.id);
+    }
+
+    const [matchArr, setMatches] = useState<Match[]>([]);
+    const [winnerId, setWinnerId] = useState<string>('');
+    const MatchPromises = useQueryMatches();
+    useEffect(() => {
+        MatchPromises.then((matches: Match[]) => {
+            setMatches(matches);
+            setWinnerId(matches[matches.length - 1].winner_id);
+        });
+    }, [MatchPromises]);
+
+    function ShowResult(props: {p1: string, p2: string}) {
+        // console.log('winnerId', winnerId);
+        if (winnerId === '1') {
+            return (
+                <h2>
+                    <div>
+                        Winner
+                        &nbsp;=&gt;
+                        { props.p1  }!!!
+                    </div>
+                </h2>
+            );
+        }
+        return (
+            <h2>
+                <div>
+                    Winner
+                    &nbsp;=&gt;
+                    {  props.p2  }!!!
+                </div>
+            </h2>
+        );
+    }
+
+    interface MatchListProps {
+        matches: Match[];
+    }
+    function MatchList({ matches }: MatchListProps) {
+        const [matchUser, setMatchUser] = useState<User>();
+        const [selectedPlayer, setSelectedPlayer] = useState('');
+        const [filteredMatches, setFilterdMatchs] = useState<Match[]>([]);
+        const MatchUserPromises = useProfileUser();
+        useEffect(() => {
+            MatchUserPromises.then((userDto: User) => {
+                setMatchUser(userDto);
+                if (matchUser) {
+                    setSelectedPlayer(matchUser.name);
+                    const createdMatchs = selectedPlayer ? matches.filter((match) =>
+                        match.player1 === selectedPlayer || match.player2 === selectedPlayer
+                    ) : matches;
+                    setFilterdMatchs(createdMatchs);
+                }
+            });
+        }, [UserPromises]);
+
+
+
+
+        return (
+            <div>
+                <h3>Result</h3>
+                {filteredMatches.map((match) => (
+                    <div key={match.id}>
+                        <h1>[{match.id}] {match.player1} vs {match.player2}</h1>
+                        <div>
+                            <ShowResult p1={match.player1} p2={match.player2} />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
     }
 
     return (
@@ -65,6 +139,7 @@ const Profile = () => {
             <Button onClick={handleButtonClick}>
                 enter
             </Button>
+            <MatchList matches={matchArr} />
         </div>
     );
 }
