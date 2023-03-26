550fee03beb09465a5448cfe56f169ff5fe0d112
kny-i <kny_i@keio.jp>
2023-03-27 07:39:52 +0900

    #129[add] add reload function

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -149,6 +149,7 @@ export class GameGateway {
       roomId = NameToInviteRoomIdDic[name];
     }
     this.server.to(roomId).emit('Ping', name, client.id);
+    console.log(name, client.id, 'Ping');
   }
 
   @SubscribeMessage('Pong')
@@ -161,6 +162,7 @@ export class GameGateway {
       roomId = NameToInviteRoomIdDic[name];
     }
     this.server.to(roomId).emit('Pong', name, client.id);
+    console.log(name, client.id, 'Pong');
   }
 
   @SubscribeMessage('ScoreToServer')
