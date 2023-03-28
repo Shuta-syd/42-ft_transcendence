bab002269809b56837d29ae1169636bca888d49e
vim <cant_reply@mail.com>
2023-03-29 07:08:56 +0900

    #164[add] add online button

--- frontend/src/components/profile/ShowAvatar.tsx
+++ frontend/src/components/profile/ShowAvatar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { styled } from '@mui/material/styles';
 import { Avatar } from "@mui/material";
+import Badge from '@mui/material/Badge';
 import { User } from "../../types/PrismaType";
 
 interface ShowAvatarProps {
@@ -7,10 +9,49 @@ interface ShowAvatarProps {
     profileImage: string;
 }
 
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+        backgroundColor: '#44b700',
+        color: '#44b700',
+        boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+        '&::after': {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            borderRadius: '50%',
+            animation: 'ripple 1.2s infinite ease-in-out',
+            border: '1px solid currentColor',
+            content: '""',
+        },
+    },
+    '@keyframes ripple': {
+        '0%': {
+            transform: 'scale(.8)',
+            opacity: 1,
+        },
+        '100%': {
+            transform: 'scale(2.4)',
+            opacity: 0,
+        },
+    },
+}));
+
 const ShowAvatar = ({ user, profileImage }: ShowAvatarProps) => {
     console.log('ShowAvatar', user, profileImage);
     return (
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <StyledBadge
+                overlap="circular"
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                variant={"dot"}
+                sx={{
+                    width: 250,
+                    height: 250,
+                    marginRight: 2
+            }}
+            >
             <Avatar
                 variant="circular"
                 color="success"
@@ -18,6 +59,7 @@ const ShowAvatar = ({ user, profileImage }: ShowAvatarProps) => {
                 src={profileImage}
                 sx={{ width: 200, height: 200, marginRight: 2 }}
             />
+            </StyledBadge>
             <div>
                 <h1
                     style={{
