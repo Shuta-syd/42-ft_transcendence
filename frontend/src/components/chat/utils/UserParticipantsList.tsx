56f7a4b7c37597cc85700a9592d78f4d8e610fe3
Shuta-syd <shuta.ogura0921@gmail.com>
2023-04-03 21:03:11 +0900

    #193 [update] Loading circle add

--- frontend/src/components/chat/utils/UserParticipantsList.tsx
+++ frontend/src/components/chat/utils/UserParticipantsList.tsx
@@ -60,7 +60,7 @@ export default function UserParticipantList(props: UserParticipantListProps) {
       } finally {
         setTimeout(() => {
           setLoading(false);
-        }, 800);
+        }, 500);
       }
     }
 
