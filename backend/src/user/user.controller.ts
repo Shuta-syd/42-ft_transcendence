a6fa1d3b856dbb5fda45194de9bc1f8da8306f28
public-jun <generalpublicjunjun@gmail.com>
2023-04-06 23:57:56 +0900

    #201 [add] block unblock getblocklist

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   Delete,
   Get,
+  HttpCode,
   HttpStatus,
   Param,
   Patch,
@@ -13,12 +14,11 @@ import {
 import { Controller } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from '@prisma/client';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { PrismaUser, SwaggerFriends } from 'src/swagger/type';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
-import { AcceptFriend, FriendReq } from './dto/user.dto';
+import { AcceptFriend, UserDto } from './dto/user.dto';
 
 @ApiTags('user')
 @Controller('user')
@@ -141,5 +141,83 @@ export class UserController {
   @Get('image')
   async getUserImage(@Req() req: Request): Promise<string> {
     return this.userService.getUserImage(req.user.id);
+  }
+
+  /**
+   * Block
+   */
+  @ApiOperation({
+    summary: 'Block a user',
+    description: 'Adds the specified user ID to the block list.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Block successful',
+    type: UserDto,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'The user is trying to block themselves',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Specified user ID not found',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Block relationship already exists',
+  })
+  @Post('block/:userId')
+  @HttpCode(HttpStatus.OK)
+  async blockUser(
+    @Req() req: Request,
+    @Param('userId') userId: string,
+  ): Promise<User> {
+    return this.userService.blockUser(req.user.id, userId);
+  }
+
+  @ApiOperation({
+    summary: 'Unblock a user',
+    description: 'Removes the specified user ID from the block list.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns the unblocked user',
+    type: UserDto,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'The user is trying to unblock themselves',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Specified user ID not found or block relationship not found',
+  })
+  @Delete('block/:userId')
+  @HttpCode(HttpStatus.OK)
+  async unblockUser(
+    @Req() req: Request,
+    @Param('userId') userId: string,
+  ): Promise<User> {
+    return this.userService.unblockUser(req.user.id, userId);
+  }
+
+  @ApiOperation({
+    summary: 'Get the block list of a user',
+    description: 'Retrieve the block list for the specified user ID.',
+  })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Returns an array of users included in the block list for the specified user ID',
+    type: [UserDto],
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Specified user ID not found',
+  })
+  @Get('block')
+  async getBlockList(@Req() req: Request): Promise<User[]> {
+    return this.userService.getBlockList(req.user.id);
   }
 }
