f7367ba457af6717e16bf5208cdd9ee4ccc8f3a4
vim <cant_reply@mail.com>
2023-04-10 21:26:51 +0900

    #227[rename] follow->friendRequest

--- frontend/src/components/profile/FriendRequestButton.tsx
+++ frontend/src/components/profile/FriendRequestButton.tsx
@@ -1,52 +1,51 @@
-import React, { useState} from "react";
-import { Button } from "@mui/material";
-import axios from "axios";
+import React, { useState } from 'react';
+import { Button } from '@mui/material';
+import axios from 'axios';
 // import fetchProfileUser from "../../hooks/profile/useProfileUser"
 import { User } from '../../types/PrismaType';
 
 interface FriendRequestButtonProps {
-    user: User | undefined;
+  user: User | undefined;
 }
 
 const FriendRequestButton = (props: FriendRequestButtonProps) => {
-    const [isFollowing, setIsFollowing] = useState(false);
-    // const [loginUser, setLoginUser] = useState<User>();
+  const [isFriendRequest, setIsFriendRequest] = useState(false);
+  // const [loginUser, setLoginUser] = useState<User>();
 
-    // /* 自分が誰なのかという情報 */
-    // useEffect(() => {
-    //     const UserPromises = fetchProfileUser();
-    //     UserPromises.then((userDto: User) => {
-    //         setLoginUser(userDto);
-    //     });
-    // }, []);
+  // /* 自分が誰なのかという情報 */
+  // useEffect(() => {
+  //     const UserPromises = fetchProfileUser();
+  //     UserPromises.then((userDto: User) => {
+  //         setLoginUser(userDto);
+  //     });
+  // }, []);
 
+  const handleClick = () => {
+    console.log('Friend Request Button');
+    setIsFriendRequest(!isFriendRequest);
 
-    const handleClick = () => {
-        console.log("Friend Request Button");
-        setIsFollowing(!isFollowing);
+    /* login user -> other peopleに対してfriend requestを送信する */
+    axios
+      .post(`http://localhost:8080/user/friendReq`, {
+        friendId: props.user?.id,
+      })
+      .then(
+        (res) => {
+          console.log('success!', res);
+        },
+        (err) => {
+          console.log('error!', err);
+        },
+      );
+  };
 
-
-        /* login user -> other peopleに対してfriend requestを送信する */
-        axios.post(`http://localhost:8080/user/friendReq`, {
-            friendId: props.user?.id,
-        }).then((res) => {
-            console.log('success!', res);
-        }, (err) => {
-            console.log('error!', err);
-        },);
-    };
-
-    return (
-        <>
-            <Button
-                variant="contained"
-                size={"large"}
-                onClick={handleClick}
-            >
-                {isFollowing ? "Unfollow" : "Follow"}
-            </Button>
-        </>
-    );
+  return (
+    <>
+      <Button variant="contained" size={'large'} onClick={handleClick}>
+        {isFriendRequest ? '' : 'Friend Request'}
+      </Button>
+    </>
+  );
 };
 
 export default FriendRequestButton;
