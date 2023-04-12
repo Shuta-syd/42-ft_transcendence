c4a32bee124c2464d58aa0cfe516452676aae8a5
kny-i <kny_i@keio.jp>
2023-04-13 07:03:24 +0900

    #257[add] add socket funcs

--- frontend/src/features/pong/index.tsx
+++ frontend/src/features/pong/index.tsx
@@ -1,5 +1,32 @@
-const Pong = () => (
-    <div>Hello</div>
-)
+import React, { useEffect } from 'react';
+import { Socket } from 'socket.io-client';
+import useSocket from '../../hooks/useSocket';
+import { fetchProfileUser } from '../../hooks/profile/useProfileUser';
+import { User } from '../../types/PrismaType';
+
+const Pong = () => {
+
+    const socket: Socket = useSocket("http://localhost:8080/pong");
+    const [user, setUser] = React.useState<User>();
+
+    const UserPromises = fetchProfileUser();
+    useEffect(() => {
+        UserPromises.then((userDto: User) => {
+            setUser(userDto);
+        });
+    }, []);
+
+    socket.on('pong', (data: string) => {
+        console.log(data);
+    }
+    );
+
+    return (
+    <div>
+        Hello
+        <h1>{user?.name}</h1>
+    </div>
+    );
+}
 
 export default Pong;
