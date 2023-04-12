4df07e169ff3b51fde1513f1bf949351a50b70e8
kny-i <kny_i@keio.jp>
2023-04-13 07:15:24 +0900

    #257[add] random match front

--- frontend/src/features/pong/index.tsx
+++ frontend/src/features/pong/index.tsx
@@ -8,7 +8,9 @@ const Pong = () => {
 
     const socket: Socket = useSocket("http://localhost:8080/pong");
     const [user, setUser] = React.useState<User>();
+    const [groupId, setGroupId] = React.useState<string>('');
 
+    // fetch user
     const UserPromises = fetchProfileUser();
     useEffect(() => {
         UserPromises.then((userDto: User) => {
@@ -16,15 +18,34 @@ const Pong = () => {
         });
     }, []);
 
-    socket.on('pong', (data: string) => {
-        console.log(data);
-    }
-    );
+    // send match request
+    useEffect(() => {
+        socket.emit('match_request');
+
+        socket.on('match_found', (groupIdDto: string) => {
+            console.log('match found! GroupId = ', groupIdDto);
+            setGroupId(groupIdDto);
+        });
+
+        return () => {
+            socket.off('match_found');
+        }
+    }, []);
+
+    useEffect(() => {
+        if (groupId === '') return;
+        socket.emit('join_group', groupId);
+    }, [groupId]);
+
 
     return (
     <div>
-        Hello
         <h1>{user?.name}</h1>
+        {groupId ? (
+            <p>Match found! Group ID: {groupId}</p>
+        ) : (
+            <p>Waiting for a match...</p>
+        )}
     </div>
     );
 }
