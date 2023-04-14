ccb35d995db45fba36032f42eab24bff82a6cb6c
vim <cant_reply@mail.com>
2023-04-14 20:16:18 +0900

    #165[add] add block button font

--- frontend/src/components/profile/BlockButton.tsx
+++ frontend/src/components/profile/BlockButton.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Button } from '@mui/material';
 import { User } from '../../types/PrismaType';
 
 interface BlockButtonProps {
   user: User | undefined;
 }
 
+const handleClick = () => {
+  console.log('Block Button');
+  return <>ok</>;
+};
+
 const BlockButton = (props: BlockButtonProps) => {
   console.log('BlockButton', props);
-  return <div>BlockButton</div>;
+  return (
+    <>
+      <Button
+        variant="outlined"
+        size={'large'}
+        color={'error'}
+        onClick={handleClick}
+      >
+        {'Block'}
+      </Button>
+    </>
+  );
 };
 
 export default BlockButton;
