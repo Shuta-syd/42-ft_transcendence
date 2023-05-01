d148cfce072f996b156f628547e2ceb5a01a7ab3
Shuta-syd <shuta.ogura0921@gmail.com>
2023-05-01 13:49:49 +0900

    #321 [fix] channel password bug fix

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -43,7 +43,7 @@ export class ChatService {
     if (dto.type === 'PROTECT') {
       const salt = randomBytes(8).toString('hex');
       const hash = (await asyncScrypt(dto.password, salt, 32)) as Buffer;
-      hashedPassword = hash.toString() + '.' + salt;
+      hashedPassword = hash.toString('base64') + '.' + salt;
     }
     const room = this.prisma.chatRoom
       .create({
@@ -280,8 +280,8 @@ export class ChatService {
 
       const hashedPassword = (
         (await asyncScrypt(dto.oldPassword, salt, 32)) as Buffer
-      ).toString();
-      if (storedHash !== hashedPassword.toString())
+      ).toString('base64');
+      if (storedHash !== hashedPassword)
         throw new UnauthorizedException('Password is wrong');
 
       const newSalt = randomBytes(8).toString('hex');
