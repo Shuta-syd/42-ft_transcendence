7435d3fd3d4be4063b8da842be91e4930e7005ab
vim <cant_reply@mail.com>
2023-05-04 10:15:17 +0900

    #352[fix]frontendでmuiのErrorが出ていたので修正

--- frontend/src/features/game/JoinInvitedRoom.tsx
+++ frontend/src/features/game/JoinInvitedRoom.tsx
@@ -46,122 +46,120 @@ const JoinInvitedRoom = (props: { socket: Socket, user: User }) => {
   };
 
   return (
-    <div
-      style={{
-        backgroundColor: '#EDF0F4',
-        minHeight: '100vh',
-        backgroundSize: 'cover',
-      }}
-    >
-      <div>
-        <h1>[Guest Room]</h1>
-        <Grid
-          container
-          justifyContent="center"
-          marginTop="-5%"
-          alignItems="center"
-          style={{ minHeight: '100vh' }}
-          fontSize="h5.fontSize"
-          direction="column"
-        >
-          {isButtonVisible && (
-            <Grid item mr={11} spacing={13}>
-              <h2
-                style={{
-                  fontSize: '2rem',
-                }}
-              >
-                Please enter your Invite ID😄
-              </h2>
-              <br />
-            </Grid>
-          )}
-          {isButtonVisible && (
-            <Grid mr={13} spacing={10}>
-              <input
-                type="text"
-                style={{
-                  borderRadius: '100px',
-                  fontSize: '2rem',
-                }}
-                value={tmpNumber}
-                onChange={handleInputChange}
-              />
-              <button
-                onClick={handleButtonClick}
-                style={{
-                  borderRadius: '100px',
-                  fontSize: '2rem',
-                }}
-              >
-                enter
-              </button>
-            </Grid>
-          )}
-          {IsAssigned && (
-            <h2
-              style={{
-                borderRadius: '100px',
-                fontSize: '4rem',
-              }}
-            >
-              You are successfully assigned !!
-            </h2>
-          )}
-          {IsUncorrect && (
-            <h2
-              style={{
-                borderRadius: '100px',
-                fontSize: '4rem',
-              }}
-            >
-              ID was WRONG!!
-            </h2>
-          )}
-          {IsAssigned && (
-            <Link
-              style={{
-                borderRadius: '100px',
-                fontSize: '4rem',
-              }}
-              to={'/game-rewrite/player2'}
-            >
-              lets go!
-            </Link>
-          )}
-          {IsAssigned && (
-            <h2
-              style={{
-                borderRadius: '100px',
-                fontSize: '4rem',
-              }}
-            >
-              You will fight against {Game?.player1}！
-            </h2>
-          )}
+      <div
+          style={{
+            backgroundColor: "#EDF0F4",
+            minHeight: "100vh",
+            backgroundSize: "cover",
+          }}
+      >
+        <div>
+          <h1>[Guest Room]</h1>
+          <Grid
+              container
+              justifyContent="center"
+              alignItems="center"
+              style={{ minHeight: "100vh" }}
+              direction="column"
+          >
+            {isButtonVisible && (
+                <Grid item container justifyContent="center">
+                  <h2
+                      style={{
+                        fontSize: "2rem",
+                      }}
+                  >
+                    Please enter your Invite ID😄
+                  </h2>
+                  <br />
+                </Grid>
+            )}
+            {isButtonVisible && (
+                <Grid container justifyContent="center">
+                  <input
+                      type="text"
+                      style={{
+                        borderRadius: "100px",
+                        fontSize: "2rem",
+                      }}
+                      value={tmpNumber}
+                      onChange={handleInputChange}
+                  />
+                  <button
+                      onClick={handleButtonClick}
+                      style={{
+                        borderRadius: "100px",
+                        fontSize: "2rem",
+                      }}
+                  >
+                    enter
+                  </button>
+                </Grid>
+            )}
+            {IsAssigned && (
+                <h2
+                    style={{
+                      borderRadius: "100px",
+                      fontSize: "4rem",
+                    }}
+                >
+                  You are successfully assigned !!
+                </h2>
+            )}
+            {IsUncorrect && (
+                <h2
+                    style={{
+                      borderRadius: "100px",
+                      fontSize: "4rem",
+                    }}
+                >
+                  ID was WRONG!!
+                </h2>
+            )}
+            {IsAssigned && (
+                <Link
+                    style={{
+                      borderRadius: "100px",
+                      fontSize: "4rem",
+                    }}
+                    to={"/game-rewrite/player2"}
+                >
+                  lets go!
+                </Link>
+            )}
+            {IsAssigned && (
+                <h2
+                    style={{
+                      borderRadius: "100px",
+                      fontSize: "4rem",
+                    }}
+                >
+                  You will fight against {Game?.player1}！
+                </h2>
+            )}
 
-          <Grid mr={10} spacing={10}>
-            <p></p>
-            <Grid mr={13} spacing={10}>
-              <button
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  height: '50px',
-                  width: '50px',
-                  fontSize: '20px',
-                  color: 'green',
-                }}
-                onClick={handleClick}
-              >
-                Exit Room
-              </button>
+            <Grid container justifyContent="center">
+              <p></p>
+              <Grid container justifyContent="center">
+                <button
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      height: "50px",
+                      width: "50px",
+                      fontSize: "20px",
+                      color: "green",
+                    }}
+                    onClick={handleClick}
+                >
+                  Exit Room
+                </button>
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
+        </div>
       </div>
-    </div>
   );
 };
 
