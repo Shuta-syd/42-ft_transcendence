7fabeeb4ffded4aaa51a873b0dbeb1cc34fef79e
public-jun <generalpublicjunjun@gmail.com>
2023-05-05 13:40:38 +0900

    #367 [fix] player assigned validation

--- backend/src/game/rewrite/game-rewrite.service.ts
+++ backend/src/game/rewrite/game-rewrite.service.ts
@@ -23,6 +23,21 @@ export class GameReWriteService {
    * 返り値はGame
    */
   async createOrJoinRandomGameAsPlayer(playerName: string): Promise<Game> {
+    // すでにassignされているランダムゲームがあるかをprisma.findUniqueで検索
+    const alreadyAssignedGame = await this.prisma.game.findFirst({
+      where: {
+        OR: [
+          {
+            player1: playerName,
+          },
+          {
+            player2: playerName,
+          },
+        ],
+      },
+    });
+    // ある場合はその値を返す
+    if (alreadyAssignedGame) return alreadyAssignedGame;
     // prisma.findFirstでplayer2プロパティに'player2'の文字列が入ったレコード1つ取り出す
     const player2NotAssginedGame = await this.prisma.game.findFirst({
       where: {
@@ -55,20 +70,7 @@ export class GameReWriteService {
    */
   async createRandomGameRoom(player1Name: string): Promise<Game> {
     // すでにassignされているランダムゲームがあるかをprisma.findUniqueで検索
-    const prismaGame = await this.prisma.game.findFirst({
-      where: {
-        OR: [
-          {
-            player1: player1Name,
-          },
-          {
-            player2: player1Name,
-          },
-        ],
-      },
-    });
-    // ある場合はその値を返す
-    if (prismaGame) return prismaGame;
+
     // awaitして同期処理に変更 thenは使用しない
     // 作成済みのゲームがない場合は新たに作成する
     // player1は引数の変数、player2は’player2_’+ uuidv4()
@@ -123,18 +125,6 @@ export class GameReWriteService {
    * player2としてRandom Gameに参加する（Gameデータベースを上書きする）
    */
   async JoinRandomGameAsPlayer2(playerName: string): Promise<Game> {
-    // 既にplayer2としてアサインされているゲームがあるかを確認
-    const alreadyAssignedRandomGameAsPlayer2 = await this.prisma.game.findFirst(
-      {
-        where: {
-          player2: playerName,
-        },
-      },
-    );
-    if (alreadyAssignedRandomGameAsPlayer2) {
-      return alreadyAssignedRandomGameAsPlayer2;
-    }
-
     // prisma.findFirstでplayer2プロパティに'player2'の文字列が入ったレコード1つ取り出す
     const player2NotAssginedGame = await this.prisma.game.findFirst({
       where: {
